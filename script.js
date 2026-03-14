c82fccfc23b1306467b69aa451cd2365caee936a
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-03-14 14:26:59 -0700

    java part 1

---
+++ script.js
@@ -0,0 +1,27 @@
+// STEP 1
+// set up/fetch weather and location APIs
+// https://open-meteo.com/
+//
+//start with Docs (website block)
+// get the API's url from the website, copy and paste
+//
+// ex:
+// https://api.open-meteo.com/v1/forecast?
+// latitude=52.52&
+// longitude=13.41&
+// daily=weather_code,temperature_2m_max,temperature_2m_min
+// &hourly=temperature_2m,weather_code
+// &current=temperature_2m,weather_code,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation
+
+// on API WEBSITE click on: API URL (Open in new tab or copy this URL into
+//  your application)
+// info
+// currently to get the API (of a city, so on a search bar you look for a city's
+//  weather)
+// you currently need "latitude" and "longitude"
+//
+//
+// HW!
+// thus we need another API 
+// In our search bar, we type in an address/city, and the api will auto get the 
+// address/city's "latitude" and "longitude"
