2ae63efef65753bec8420e659eeeb49640c303c8
Erik Nieto Reynoso <erik.nieto101@gmail.com>
2026-03-15 16:27:55 -0700

    java weather and location api end

--- script.js
+++ script.js
@@ -22,17 +22,87 @@
 //
 //
 // HW!
-// thus we need another API 
-// In our search bar, we type in an address/city, and the api will auto get the 
+// thus we need another API
+// In our search bar, we type in an address/city, and the api will auto get the
 // address/city's "latitude" and "longitude"
 //!g site:reddit.com/r/webdev .......[ask your question]...
 
-//!g site:reddit.com/r/webdev geolocation for latitude and longitude from address 
+//!g site:reddit.com/r/webdev geolocation for latitude and longitude from address
 
 // I found  "Nominatim search"
 // now fine the API
 //   https://nominatim.openstreetmap.org/search?
 
 // search: mdn javascript get api
-// now you get a template
+//MDN stands for Mozilla Developer Network, which is a comprehensive resource for web development documentation covering technologies like HTML, CSS, and JavaScript. It provides guides, tutorials, and references for developers of all skill
+// now you get a template:
+
+//async function getData() {
+//   const url = "https://example.org/products.json";
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
+
+//     const result = await response.json();
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+
+// use template literals/ Embedded Expressions,  by backtick (`), ${}
+//now we have the long and lat
+
+//next get the Weather API
+//template literals/ Embedded Expressions,  by backtick (`), ${}
+
+//connect getGeoData() and getWeatherData()
+
+
+async function getGeoData() {
+    let search = "los angeles, ca"
+
+
+  const url = 
+  `https://nominatim.openstreetmap.org/search?q=${search}&format=jsonv2`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+    
+    let lat = result[0].lat;
+    let lon = result[0].lon;
+    getWeatherData(lat, lon);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+getGeoData();
+
+
+
+async function getWeatherData(lat, lon) {
+
+
+  const url = 
+  `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=weather_code,temperature_2m_max,temperature_2m_min&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code,apparent_temperature,relative_humidity_2m,wind_speed_10m,precipitation`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
